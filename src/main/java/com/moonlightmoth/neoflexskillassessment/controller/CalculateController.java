d63c7d854909e36b8016a67ba572f5682afbe21a
moonlightmoth <ghjpa@inbox.ru>
2024-09-18 02:28:35 +0300

    in progress

--- src/main/java/com/moonlightmoth/neoflexskillassessment/controller/CalculateController.java
+++ src/main/java/com/moonlightmoth/neoflexskillassessment/controller/CalculateController.java
@@ -1,7 +1,9 @@
 package com.moonlightmoth.neoflexskillassessment.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,14 +14,18 @@ public class CalculateController {
     private static final int FULL_FORM = 1;
 
     @GetMapping("/calculate")
-    public ResponseEntity<String> calculate(String avgSalary, String vacationLength, String fromDate, String dueToDate)
+    public ResponseEntity<String> calculate(
+            @RequestParam (name = "avgSalary", required = false)                String avgSalary,
+            @RequestParam (name = "vacationLength", required = false)           String vacationLength,
+            @RequestParam (name = "fromDate", required = false)                 String fromDate,
+            @RequestParam (name = "dueToDate", required = false)                String dueToDate)
     {
         int type = getRequestType(avgSalary, vacationLength, fromDate, dueToDate);
         ResponseEntity<String> resp;
 
         switch (type)
         {
-            default -> resp = ResponseEntity.badRequest().build();
+            default -> resp = ResponseEntity.status(HttpStatus.OK).body("Request must have query OR body params avgSalary AND (vacationLength OR (fromDate AND dueToDate))");
         }
 
         return resp;
@@ -30,7 +36,9 @@ public class CalculateController {
         if (avgSalary == null)
             return INVALID_ARGS;
 
-        if (vacationLength == null && fromDate == null || dueToDate == null) //TODO
+        if (vacationLength == null && (fromDate == null || dueToDate == null)) //TODO
             return INVALID_ARGS;
+
+        return INVALID_ARGS;
     }
 }
